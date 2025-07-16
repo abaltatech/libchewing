e654a0bbd6ecfc59f7b56675559cd311b40dfe23
Rumen Cholakov <rcholakov@abaltatech.com>
2025-07-16 21:01:14 +0300

    Switch to dynamic library and update data files
    Changed the Chewing library product type from static to dynamic in Package.swift. Updated Info.plist to swap architecture/platform entries for the xcframework. Refreshed binary data files: mini.dat, tsi.dat, and word.dat.

--- swift/chewing-simplified/Package.swift
+++ swift/chewing-simplified/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "Chewing",
-            type: .static,
+            type: .dynamic,
             targets: ["Chewing"]
         ),
     ],
